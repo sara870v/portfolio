47b967e241506a35a42f5331f1e3d9a127defd77
Sarah Davidsen <sarahdavidsen@Sarahs-MacBook-Pro-2.local>
2019-10-13 21:50:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let dropcontent = document.querySelector(".dropdown-content");
+
+let menu = document.querySelector("#menu");
+let overlay = document.querySelector(".overlay");
+let overlaycontent = document.querySelector(".overlaycontent");
+let exit = document.querySelector(".closebtn");
+
+menu.addEventListener("click", showOverlay);
+
+function showOverlay() {
+    overlay.style.width = "250px";
+    overlaycontent.style.display = "block";
+    exit.style.display = "block";
+    menu.style.visibility = "hidden";
+
+    exit.addEventListener("click", closeOverlay);
+
+}
+
+function closeOverlay() {
+    overlay.style.width = "0%";
+    overlaycontent.style.display = "none";
+    exit.style.display = "none";
+    menu.style.visibility = "visible";
+}
