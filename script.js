969872fd5b116c3282cfc97b92baf937cacf4c33
Sarah Davidsen <sarahdavidsen@laptop-vmaha7vp.kea.local>
2019-10-24 10:52:00 +0200

    tekster

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", start);
+function start() {
+  console.log("Hej");
+  activateButton();
+}
+
+function activateButton() {
+  document.querySelector(".aboutme_btn").addEventListener("click", () => {
+    window.location.href = "om.html";
+  });
+
+  document.querySelector(".cv_btn").addEventListener("click", () => {
+    window.location.href = "cv.html";
+  });
+}
+
 let dropcontent = document.querySelector(".dropdown-content");
 
 let menu = document.querySelector("#menu");
@@ -8,18 +25,17 @@ let exit = document.querySelector(".closebtn");
 menu.addEventListener("click", showOverlay);
 
 function showOverlay() {
-    overlay.style.width = "250px";
-    overlaycontent.style.display = "block";
-    exit.style.display = "block";
-    menu.style.visibility = "hidden";
+  overlay.style.width = "250px";
+  overlaycontent.style.display = "block";
+  exit.style.display = "block";
+  menu.style.visibility = "hidden";
 
-    exit.addEventListener("click", closeOverlay);
-
+  exit.addEventListener("click", closeOverlay);
 }
 
 function closeOverlay() {
-    overlay.style.width = "0%";
-    overlaycontent.style.display = "none";
-    exit.style.display = "none";
-    menu.style.visibility = "visible";
+  overlay.style.width = "0%";
+  overlaycontent.style.display = "none";
+  exit.style.display = "none";
+  menu.style.visibility = "visible";
 }
